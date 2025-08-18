00ec52dfbc06d23069823b3ccb2a5b3f1a1fae0a
sajedul559 <sajedulkhairul@gmail.com>
2025-08-18 08:29:16 +0600

    working on frontend

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route,Routes} from 'react-router-dom';
+import { Home } from './components/frontend/Home';
+import About from './components/frontend/About';
+import './assets/css/style.scss'
 
 
 
@@ -11,7 +14,12 @@ function App() {
 
   return (
     <>
-        <button className='btn btn-primary'> Click Here</button>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home/>} />
+                <Route path='/about' element={<About/>} />
+            </Routes>
+        </BrowserRouter>
     </>
   )
 }
